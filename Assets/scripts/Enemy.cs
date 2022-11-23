5112b12c5552d5907e51a02fe17c3c4ff6a2aa8b
Holger <115777904+Holger7777@users.noreply.github.com>
2022-11-23 16:13:06 +0100

    player controls and collision

---
+++ Assets/scripts/Enemy.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+/* Code til enemy AI https:// www.youtube.com/watch?v=ZExSz7x69j8&t=1s */
+
+
+
+public class Enemy : MonoBehaviour
+{
+    public static event Action<Enemy> OnEnemyKilled;
+    [SerializeField] private float health, maxHealth = 3f;
+
+    [SerializeField] private float moveSpeed = 2f;
+    private Rigidbody2D rb;
+    private Transform target;
+    private Vector2 moveDirection;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        health = maxHealth;
+        target = GameObject.Find("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            rb.rotation = angle;
+            moveDirection = direction;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        Debug.Log($"DamageAmount: {damageAmount}");
+        health -= damageAmount;
+        Debug.Log($"Health is now {health}");
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+            OnEnemyKilled?.Invoke(this);
+        }
+        
+    }
+  
+}
